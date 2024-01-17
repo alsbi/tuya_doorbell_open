00988f3e81002335f718f982427d8617ddc4876a
alsbi <feanor4ik@gmail.com>
2024-01-17 22:38:07 +0300

    Update button.py

--- custom_components/tuya_doorbell_open/button.py
+++ custom_components/tuya_doorbell_open/button.py
@@ -4,7 +4,7 @@ import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 from homeassistant.components.button import ButtonEntity
 from homeassistant.helpers.config_validation import (PLATFORM_SCHEMA)
-from tuya_connector import  TuyaOpenAPI
+from tuya_connector import TuyaOpenAPI
 
 from .const import *
 
@@ -14,7 +14,9 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(ACCESS_ID): cv.string,
     vol.Required(ACCESS_KEY): cv.string,
     vol.Required(DEVICE_ID): cv.string,
+    vol.Optional(CONF_NAME): cv.string,
 })
+
 
 async def async_setup_platform(
         hass,
@@ -24,25 +26,29 @@ async def async_setup_platform(
 ) -> None:
     button = TuyaDoorBellOpen(config.get(ACCESS_ID),
                               config.get(ACCESS_KEY),
-                              config.get(DEVICE_ID)
+                              config.get(DEVICE_ID),
+                              name=config.get(CONF_NAME)
                               )
 
     async_add_entities([button], update_before_add=False)
 
 
 class TuyaDoorBellOpen(ButtonEntity):
-    def __init__(self, access_id, access_key, device_id):
+    def __init__(self, access_id, access_key, device_id, name='dorrbell'):
         self.access_id = access_id
         self.access_key = access_key
         self.device_id = device_id
 
+        self._name = name
+        self.no_domain_ = self._name.startswith("!")
+        if self.no_domain_:
+            self._name = self.name[1:]
         self._unique_id = self._name.lower().replace(' ', '_')
 
-        self.openapi = TuyaOpenAPI(API_ENDPOINT, self.access_id, self.access_key)
-        self.openapi.connect()
-
     def press(self) -> None:
-        response = self.openapi.post(
+        openapi = TuyaOpenAPI(API_ENDPOINT, self.access_id, self.access_key)
+        openapi.connect()
+        response = openapi.post(
             f"/v2.0/cloud/thing/{self.device_id}/shadow/properties/issue", {
                 "properties": {
                     "accessory_lock": True
