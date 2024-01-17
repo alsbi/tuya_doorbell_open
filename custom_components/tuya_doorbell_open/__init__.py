10b459eb8482c2365e64a0df3d450077fa7a3dc4
alsbi <feanor4ik@gmail.com>
2024-01-17 21:18:38 +0300

    __init__.py

--- custom_components/tuya_doorbell_open/__init__.py
+++ custom_components/tuya_doorbell_open/__init__.py
@@ -1,1 +1,15 @@
+from __future__ import annotations
 
+import logging
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    _LOGGER.debug(f'{DOMAIN} setup')
+    return True
